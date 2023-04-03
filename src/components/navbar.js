459267fe5667d857d684d6e76d23c34c665cb384
polymorphicus <120289616+polymorphicus@users.noreply.github.com>
2023-04-02 23:53:07 -0400

    Set up navbar  with styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,21 @@
 import "../styles/navbar.css"
+import {Link} from "react-router-dom"
+import CartImage from "../images/cart.svg"
 
 const Navbar = () => {
+    let filled = true;
+    let count = 1;
     return (
         <div className="navbar">
             <h2>Diamond Ace Games</h2>
             <div className="choices">
-                <h3>Home</h3>
-                <h3>Shop</h3>
-                <h3>Cart</h3>
+                <Link to="/">Home</ Link>
+                <Link to="/shop">Shop</Link>
+                <div className="cart--part">
+                    <Link to="/cart">Cart</Link>
+                    <img src={CartImage} alt="Shopping cart" style={{ width:"30px", height:"30px"}}/>
+                    {filled && <div className="amount">{count}</div>}
+                </div>
             </div>
         </div>
     )  
